4e0ff2cef20370b7f0f875538bea9e09f17d5276
Yernar <eraga0423@gmail.com>
2024-08-25 00:44:18 +0500

    fix:chatbot

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mymmrac/telego"
+	tu "github.com/mymmrac/telego/telegoutil"
+)
+
+func main() {
+	botToken := "7261966141:AAGw7-WCqfMpHpkXZEgDNBGvX-bjN7JdpcI"
+	bot, err := telego.NewBot(botToken, telego.WithDefaultLogger())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	updates, _ := bot.UpdatesViaLongPollin(nil)
+
+	defer bot.StopLongPolling()
+
+	for update := range updates {
+		if update.Message != nil {
+			chatID := tu.ID(update.Message.Chat.ID)
+
+			_, _ = bot.CopyMessage(
+				tu.CopyMessage(
+					chatID,
+					chatID,
+					update.Message.MessageID,
+				),
+			)
+
+		}
+	}
+}
