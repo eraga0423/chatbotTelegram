babe9037743b7a40459249eab21e4a834d9a6384
eraga <eraga0423@gmail.com>
2024-12-29 15:10:05 +0500

    fix

--- main.go
+++ main.go
@@ -47,9 +47,8 @@ func main() {
 }
 
 func Chatgpt8ff(question string) string {
-	gtp := os.Getenv("TOKEN_GPT")
+
 	api, err := gpt.Init(gpt.Params{
-		API_TOKEN:    gtp,
 		StripNewline: true,
 		Request: gpt.ChatRequest{
 			Model: "gpt-3.5-turbo",
